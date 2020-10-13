bf275b41c7a41bc03ce36f087498fe2579e358b9
TK-KT <72793363+TK-KT@users.noreply.github.com>
2020-10-13 21:38:51 +0900

    Update index.php
    bingoCardクラスのメンバに直接触れないように変更、その後メンバをprivateに

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // ビンゴカードの判定クラス
 class BingoCard{
-    public  $s,             // ビンゴカードの一辺の長さ
+    private $s,             // ビンゴカードの一辺の長さ
             $line,          // 行に該当する単語に印が付いたとき、その座標値を行ごとに足しこむ配列
             $column,        // 列に該当する単語に印が付いたとき、その座標値を列ごとに足しこむ配列
             $upperRight,    // 左下から右上斜め行に該当する単語に印が付いたとき、その座標値を足しこむ変数
@@ -15,6 +15,27 @@ class BingoCard{
         $this->column = array_fill(1, $s+1, 0);
         $this->upperRight = 0;
         $this->lowerRight = 0;
+    }
+
+    // ビンゴカードの一辺の長さを取得する関数
+    public function getS(){
+        return $this->s;
+    }
+
+    // 座標値を受け取り、対応する配列に足しこむ関数
+    public function sumUpGridArray($x, $y){
+        // 存在した単語のx座標値を、対応する配列に足しこむ
+        $this->line[$x] += $x;
+        // 存在した単語のy座標値を、対応する配列に足しこむ
+        $this->column[$y] += $y;
+        // 存在した単語のx座標値とy座標値が等しい場合、右下斜めの変数に足しこむ
+        if ($x == $y) {
+            $this->lowerRight += $x;
+        }
+        // 存在した単語のx座標値とy座標値の和が、カードの一辺の長さ＋１と等しい場合、右上斜めの変数に足しこむ
+        if (($x+$y) == ($this->s + 1)) {
+            $this->upperRight += $x;
+        }
     }
 
     // ビンゴが成立しているかどうか判定する関数
@@ -55,7 +76,7 @@ class BingoCard{
 $bingoCard = new BingoCard(trim(fgets(STDIN)));
 
 // 元となるカードの作成(単語と座標の対応表)
-for ($y=0; $y<$bingoCard->s; $y++) {
+for ($y=0; $y<$bingoCard->getS(); $y++) {
     $inputArray = explode(' ', trim(fgets(STDIN)));
     foreach($inputArray as $x => $value) {
         $card[$value] = ['x' => $x+1, 'y' => $y+1];
@@ -68,21 +89,10 @@ $n = trim(fgets(STDIN));
 // N個分の入力を受けとる
 for ($i=0; $i<$n; $i++) {
     $inputValue = trim(fgets(STDIN));
-    // 入力された値がビンゴカード上に存在している場合
+    // 入力された値がビンゴカード上に存在している場合、座標値を対応する変数に格納する
     if (isset($card[$inputValue])) {
-        // 存在した単語のx座標値を、対応する配列に足しこむ
-        $bingoCard->line[$card[$inputValue]['x']] += $card[$inputValue]['x'];
-        // 存在した単語のy座標値を、対応する配列に足しこむ
-        $bingoCard->column[$card[$inputValue]['y']] += $card[$inputValue]['y'];
-        // 存在した単語のx座標値とy座標値が等しい場合、右下斜めの変数に足しこむ
-        if ($card[$inputValue]['x'] == $card[$inputValue]['y']) {
-            $bingoCard->lowerRight += $card[$inputValue]['x'];
-        }
-        // 存在した単語のx座標値とy座標値の和が、カードの一辺の長さ＋１と等しい場合、右上斜めの変数に足しこむ
-        if (($card[$inputValue]['x']+$card[$inputValue]['y']) == ($bingoCard->s+1)) {
-            $bingoCard->upperRight += $card[$inputValue]['x'];
-        }
-    }    
+        $bingoCard->sumUpGridArray($card[$inputValue]['x'], $card[$inputValue]['y']);
+    }
 }
 
 // ビンゴが成立しているか判定する

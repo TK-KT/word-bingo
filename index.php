92553c438ccae1b9cd2c2e2b3f579ca4ea2ead9b
TK-KT <72793363+TK-KT@users.noreply.github.com>
2020-10-14 13:13:03 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,64 +3,69 @@
 // ビンゴカードの判定クラス
 class BingoCard{
     private $s,             // ビンゴカードの一辺の長さ
+            $card,         // ビンゴカード(単語と座標の対応表)
             $line,          // 行に該当する単語に印が付いたとき、その座標値を行ごとに足しこむ配列
             $column,        // 列に該当する単語に印が付いたとき、その座標値を列ごとに足しこむ配列
             $upperRight,    // 左下から右上斜め行に該当する単語に印が付いたとき、その座標値を足しこむ変数
             $lowerRight;    // 左上から右下斜め行に該当する単語に印が付いたとき、その座標値を足しこむ変数
 
     // コンストラクタ
-    public function __construct($s){
+    public function __construct($s, $card){
         $this->s = $s;
+        $this->card = $card;
         $this->line = array_fill(1, $s+1, 0);
         $this->column = array_fill(1, $s+1, 0);
         $this->upperRight = 0;
         $this->lowerRight = 0;
     }
 
-    // ビンゴカードの一辺の長さを取得する関数
-    public function getS(){
-        return $this->s;
+    // 与えられた単語がビンゴカード上に存在するかチェックする関数
+    public function checkExistOnCard($word){
+        // 入力された値がビンゴカード上に存在している場合、座標値を対応する変数に格納する
+        if (isset($this->card[$word])) {
+            $this->countUpGridArray($this->card[$word]['x'], $this->card[$word]['y']);
+        }
     }
 
-    // 座標値を受け取り、対応する配列に足しこむ関数
-    public function sumUpGridArray($x, $y){
-        // 存在した単語のx座標値を、対応する配列に足しこむ
-        $this->line[$x] += $x;
-        // 存在した単語のy座標値を、対応する配列に足しこむ
-        $this->column[$y] += $y;
+    // 座標値を受け取り、対応する配列をカウントアップ関数
+    public function countUpGridArray($x, $y){
+        // 対応する行配列に足しこむ
+        $this->line[$x]++;
+        // 対応する列配列に足しこむ
+        $this->column[$y]++;
         // 存在した単語のx座標値とy座標値が等しい場合、右下斜めの変数に足しこむ
         if ($x == $y) {
-            $this->lowerRight += $x;
+            $this->lowerRight++;
         }
         // 存在した単語のx座標値とy座標値の和が、カードの一辺の長さ＋１と等しい場合、右上斜めの変数に足しこむ
         if (($x+$y) == ($this->s + 1)) {
-            $this->upperRight += $x;
+            $this->upperRight++;
         }
     }
 
     // ビンゴが成立しているかどうか判定する関数
     public function judgement(){
-        // 左下から右上斜め行に該当する座標値の和が、1からS(ビンゴカードの一辺の長さ)までの和と等しい場合、ビンゴ成立
-        if ($this->upperRight == ($this->s * ($this->s + 1) / 2)) {
+        // 左下から右上斜め行に該当するカウントアップした和が、S(ビンゴカードの一辺の長さ)と等しい場合、ビンゴ成立
+        if ($this->upperRight == $this->s) {
             print "yes";
             return;
         }
 
-        // 左上から右下斜め行に該当する座標値の和が、1からS(ビンゴカードの一辺の長さ)までの和と等しい場合、ビンゴ成立
-         if ($this->lowerRight == ($this->s * ($this->s + 1) / 2)) {
+        // 左上から右下斜め行に該当するカウントアップした和が、S(ビンゴカードの一辺の長さ)と等しい場合、ビンゴ成立
+         if ($this->lowerRight == $this->s) {
             print "yes";
             return;
         }
 
         // 行と列ごとに判定するループ
         for ($i=1; $i<=$this->s; $i++) {
-            // 該当行に対応する座標値の和が、該当行の座標値とS(ビンゴカードの一辺の長さ)の積と等しい場合、ビンゴ成立
-            if ($this->line[$i] == ($i * $this->s)) {
+            // 該当行に対応するカウントアップした和が、S(ビンゴカードの一辺の長さ)と等しい場合、ビンゴ成立
+            if ($this->line[$i] == $this->s) {
                 print "yes";
                 return;
             }
-            // 該当列に対応する座標値の和が、該当列の座標値とS(ビンゴカードの一辺の長さ)の積と等しい場合、ビンゴ成立
-            if ($this->column[$i] == ($i * $this->s)) {
+            // 該当列に対応するカウントアップした和が、S(ビンゴカードの一辺の長さ)と等しい場合、ビンゴ成立
+            if ($this->column[$i] == $this->s) {
                 print "yes";
                 return;
             }
@@ -72,27 +77,27 @@ class BingoCard{
     }
 }
 
-// ビンゴカードの一辺の長さ(S)を受け取り、判定クラスの作成
-$bingoCard = new BingoCard(trim(fgets(STDIN)));
+// ビンゴカードの一辺の長さ(S)を受け取る
+$s = trim(fgets(STDIN));
 
 // 元となるカードの作成(単語と座標の対応表)
-for ($y=0; $y<$bingoCard->getS(); $y++) {
-    $inputArray = explode(' ', trim(fgets(STDIN)));
-    foreach($inputArray as $x => $value) {
-        $card[$value] = ['x' => $x+1, 'y' => $y+1];
+for ($y=0; $y<$s; $y++) {
+    $wordArray = explode(' ', trim(fgets(STDIN)));
+    foreach($wordArray as $x => $word) {
+        $card[$word] = ['x' => $x+1, 'y' => $y+1];
     }
 }
+
+// ビンゴカード判定クラスの作成
+$bingoCard = new BingoCard($s, $card);
 
 // 選ばれる単語の数 N
 $n = trim(fgets(STDIN));
 
 // N個分の入力を受けとる
 for ($i=0; $i<$n; $i++) {
-    $inputValue = trim(fgets(STDIN));
-    // 入力された値がビンゴカード上に存在している場合、座標値を対応する変数に格納する
-    if (isset($card[$inputValue])) {
-        $bingoCard->sumUpGridArray($card[$inputValue]['x'], $card[$inputValue]['y']);
-    }
+    // 標準入力で受け取った単語がビンゴカード上に存在するかどうか判定する
+    $bingoCard->checkExistOnCard(trim(fgets(STDIN)));
 }
 
 // ビンゴが成立しているか判定する
